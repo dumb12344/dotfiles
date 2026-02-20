0701ec324d9f7e603cd273211d254665c4d02f83
dumb12344 <167947276+dumb12344@users.noreply.github.com>
2026-02-20 11:06:53 -0800

    fixes

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 echo Installing script packages and updating system
-sudo pacman -Syu figlet jq git base-devel niri zsh xdg-desktop-portal-gnome xwayland-satellite kitty cliphist cava xdg-desktop-portal brightnessctl ly --needed
+sudo pacman -Syu figlet jq git base-devel niri zsh xdg-desktop-portal-gnome xwayland-satellite kitty cliphist cava xdg-desktop-portal brightnessctl ly neovim --needed
 sudo systemctl enable ly@tty1.service
 sudo systemctl disable getty@tty1.service
 figlet Stuff -f big
-echo Installing yay aur helper
-yaytmp=$(mktemp -d)
-git clone https://aur.archlinux.org/yay-bin.git $yaytmp
-cd $yaytmp
-makepkg -si
-cd ~
-sudo rm -rf $yaytmp
+if [ ! -f /usr/bin/yay ]; then
+    echo Installing yay aur helper
+    yaytmp=$(mktemp -d)
+    git clone https://aur.archlinux.org/yay-bin.git $yaytmp
+    cd $yaytmp
+    makepkg -si
+    cd ~
+    sudo rm -rf $yaytmp
+fi
 echo Installing aur packages
-yay -S noctalia-shell zsh-theme-powerlevel10k-git pokeget
+yay -S noctalia-shell zsh-theme-powerlevel10k-git pokeget --needed
 echo copying configs
-cp -i -r configs/* ~/
+cp -rf configs/ ~/
 chown -R $(whoami) ~/*
 chgrp -R $(whoami) ~/*
