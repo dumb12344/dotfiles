9b60809821ea14d0c7af4a1c13cd82ca76f3c3d9
dumb12344 <167947276+dumb12344@users.noreply.github.com>
2026-02-23 12:41:19 -0800

    fixes and wallpapers

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo Installing script packages and updating system
-sudo pacman -Syu figlet jq git base-devel niri zsh xdg-desktop-portal-gnome xwayland-satellite kitty cliphist cava xdg-desktop-portal brightnessctl ly neovim --needed
+sudo pacman -Syu figlet jq git base-devel niri zsh xdg-desktop-portal-gnome \
+    xwayland-satellite kitty cliphist cava xdg-desktop-portal brightnessctl \
+    xdg-utils vulkan-radeon vulkan-intel vulkan-headers vulkan-tools ly neovim \
+    ttf-cascadia-code-nerd --needed
 sudo systemctl enable ly@tty1.service
 sudo systemctl disable getty@tty1.service
 figlet Stuff -f big
@@ -14,9 +17,12 @@ if [ ! -f /usr/bin/yay ]; then
     cd $dir
     sudo rm -rf $yaytmp
 fi
+echo Removing debug from makepkg config
+sudo sed -ie 's/purge debug/purge !debug/' /etc/makepkg.conf
 echo Installing aur packages
 yay -S noctalia-shell zsh-theme-powerlevel10k-git pokeget --needed
-echo copying configs
+echo Copying configs
 cp -rf configs/. ~
+sudo chsh test -s /bin/zsh
 chown -R $(whoami) ~/*
 chgrp -R $(whoami) ~/*
