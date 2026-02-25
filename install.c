dcc6fabf351ad23d0aeb7df125a961ec479699e9
dumb12344 <167947276+dumb12344@users.noreply.github.com>
2026-02-25 09:46:40 -0800

    why is s2=(null)

--- install.c
+++ install.c
@@ -20,11 +20,49 @@ char *choices[] = {
     "Exit",
     (char *)NULL,
 };
+char *choiceids[] = {
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    (char *)NULL,
+};
+bool finishedtasks[] = {
+    true,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+};
 void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color);
 
-int main()
-{	ITEM **my_items;
-	int c;				
+
+char* concat(char *s1, char *s2){
+    /*char *result = malloc(strlen(s1) + strlen(s2) + 1);
+    // https://stackoverflow.com/questions/8465006/how-do-i-concatenate-two-strings-in-c
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;*/
+    char *result;
+    snprintf(result, sizeof(result), "%s%s", s1, s2);
+    return result;
+}
+
+int main(){
+    ITEM **my_items;
+	int c;
 	MENU *my_menu;
     WINDOW *my_menu_win;
     int n_choices, i;
@@ -38,19 +76,21 @@ int main()
     n_choices = ARRAY_SIZE(choices);
     my_items = (ITEM **)calloc(n_choices, sizeof(ITEM *));
     for(i = 0; i < n_choices; ++i){
-        my_items[i] = new_item(choices[i], choices[i]);
+        char* prefix = concat(finishedtasks[i] ? "ee " : "ea ", choiceids[i]);
+        my_items[i] = new_item(prefix, choices[i]);
+        //my_items[i] = new_item(choiceids[i], choices[i]);
     }
     int x,y;
     getmaxyx(stdscr, x, y);
 	my_menu = new_menu((ITEM **)my_items);
-    my_menu_win = newwin(x/2, y/3, x/2, y/2);
+    my_menu_win = newwin(x/2, y/3, x/3, y/3);
     keypad(my_menu_win, TRUE);
     set_menu_win(my_menu, my_menu_win);
-    set_menu_sub(my_menu, derwin(my_menu_win, 6, 38, 3, 1));
-    set_menu_format(my_menu, 5, 1);
+    set_menu_sub(my_menu, derwin(my_menu_win, 0, 48, 3, x/3));
+    set_menu_format(my_menu, 12, 1);
     set_menu_mark(my_menu, " * ");
     box(my_menu_win, 0, 0);
-	print_in_middle(my_menu_win, 1, 0, y/3, "My Menu", COLOR_PAIR(1));
+	print_in_middle(my_menu_win, 1, 0, y/3, "My Menu", COLOR_PAIR(2));
 	mvwaddch(my_menu_win, 2, 0, ACS_LTEE);
 	mvwhline(my_menu_win, 2, 1, ACS_HLINE, y/3-2);
 	mvwaddch(my_menu_win, 2, y/3-1, ACS_RTEE);
@@ -62,7 +102,8 @@ int main()
 	attroff(COLOR_PAIR(2));
 	refresh();
 
-	while((c = wgetch(my_menu_win)) != 'q'){
+	while(c != 'q'){
+        c = wgetch(my_menu_win);
         switch(c){
             case KEY_DOWN:
 				menu_driver(my_menu, REQ_DOWN_ITEM);
@@ -72,7 +113,6 @@ int main()
 				break;
             case KEY_ENTER:
                 system("echo asd");
-                c='q';
                 break;
 		}
         wrefresh(my_menu_win);
@@ -85,8 +125,8 @@ int main()
 	endwin();
 }
 
-void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color)
-{	int length, x, y;
+void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color){
+    int length, x, y;
 	float temp;
 
 	if(win == NULL)
@@ -100,7 +140,7 @@ void print_in_middle(WINDOW *win, int starty, int startx, int width, char *strin
 		width = 80;
 
 	length = strlen(string);
-	temp = (width - length)/ 2;
+	temp = (width - length)>>1;
 	x = startx + (int)temp;
 	wattron(win, color);
 	mvwprintw(win, y, x, "%s", string);
