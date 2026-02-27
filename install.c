438404b01292bdbb2ee17241e5cce3ab442cc400
dumb12344 <167947276+dumb12344@users.noreply.github.com>
2026-02-26 17:37:40 -0800

    yes i know my code is bad (wip)

--- install.c
+++ install.c
@@ -2,6 +2,7 @@
 #include <menu.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 #define CTRLD 	4
@@ -10,7 +11,7 @@ char *choices[] = {
     "Install packages",
     "Install yay AUR helper",
     "Install AUR packages",
-    "Copy configs",
+    "Apply configs",
     "Reboot",
     "Exit",
     (char *)NULL,
@@ -94,6 +95,7 @@ int main(){
     int x,y;
     getmaxyx(stdscr, x, y);
 	menu = new_menu((ITEM **) items);
+    menu_opts_off(menu, O_ONEVALUE);
     menu_win = newwin(x / 2, y / 3, x / 3, y / 3);
     keypad(menu_win, TRUE);
     set_menu_win(menu, menu_win);
@@ -129,7 +131,8 @@ int main(){
                 id = strtol(item_name(current_item(menu)), &endptr, 10);
                 switch(id){
                     case 1:
-                        printf("Installing packages and updating system");
+                        system("clear");
+                        printw("Installing packages and updating system");
                         //install packages
                         system("sudo pacman -Syu figlet jq git base-devel niri zsh xdg-desktop-portal-gnome \
                                         xwayland-satellite kitty cliphist cava xdg-desktop-portal brightnessctl \
@@ -138,26 +141,56 @@ int main(){
                         );
                         //enable ly
                         system("sudo systemctl enable ly@tty1.service && sudo systemctl disable getty@tty1.service");
-                        finishedtasks[0] = true;
+                        system("read -n 1 -p \"Press any key to continue...\"");
+                        menu_driver(menu, REQ_TOGGLE_ITEM);
                         break;
                     case 2:
-                        system("echo 2");
-                        finishedtasks[1] = true;
-                        determineitems(items, n_choices);
-                        unpost_menu(menu);
-                        post_menu(menu);
-                        box(menu_win, 0, 0);
-                        wrefresh(menu_win);
-                        //whyyyyy
+                        if (access("/usr/bin/yay", F_OK) == 0&&false) {
+                            system("echo yay is already installed.");
+                        } else {
+                            system("echo yay is not installed.");
+                            //I don't need debug packages
+                            system("echo Disabling debug in makepkg.conf");
+                            system("sudo sed -ie 's/purge debug/purge !debug/' /etc/makepkg.conf");
+                            //create temp directory
+                            system("echo Creating temporary directory");
+                            char template[] = "/tmp/tmpdir.XXXXXX";
+                            char *dir_name = mkdtemp(template);
+                            char tmp[1000] = "echo Created directory ";
+                            strcat(tmp, dir_name);
+                            system(tmp);
+                            //clone into temp directory and save current directory
+                            char command[1000] = "git clone https://aur.archlinux.org/yay-bin.git ";
+                            strcat(command, dir_name);
+                            system(command);
+                            char dotsdir[1000];
+                            getcwd(dotsdir, sizeof(dotsdir));
+                            //make package and go back to previous directory
+                            chdir(dir_name);
+                            system("makepkg -si");
+                            chdir(dotsdir);
+                            //remove package directory
+                            char command2[1000] = "rm -rf ";
+                            strcat(command2, dir_name);
+                            system(command2);
+                        }
+                        system("read -n 1 -p \"Press any key to continue...\"");
+                        menu_driver(menu, REQ_TOGGLE_ITEM);
                         break;
                     case 3:
-                        system("echo 3 && read");
+                        system("echo Installing AUR packages");
+                        system("yay -S noctalia-shell zsh-theme-powerlevel10k-git pokeget --needed");
+                        system("read -n 1 -p \"Press any key to continue...\"");
                         break;
                     case 4:
-                        system("echo 4 && read");
+                        system("echo Applying configs");
+                        system("echo Changing shell to zsh");
+                        system("sudo chsh test -s /bin/zsh");
+                        system("echo Copying user configs");
+                        system("read -n 1 -p \"Press any key to continue...\"");
                         break;
                     case 5:
-                        system("echo 5 && read");
+                        system("reboot");
                         break;
                     case 6:
                         end = true;
