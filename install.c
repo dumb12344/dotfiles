356f7df4d2be59c9c9f20e15cd8c96e39454b714
dumb12344 <167947276+dumb12344@users.noreply.github.com>
2026-07-18 07:03:40 -0700

    too little mirrors

--- install.c
+++ install.c
@@ -134,7 +134,7 @@ void updateMirrors() {
         execute("sudo pacman -Syu reflector");
     }
     info("Updating mirrors");
-    execute("sudo reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist");
+    execute("sudo reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist");
 }
 
 void installPackages() {
